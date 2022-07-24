47e068a47dbaec662c4f4309e10816d7db7e49f4
SachinCoder1 <sachinhlo232@gmail.com>
2022-07-24 13:26:07 +0530

    hardhat init

---
+++ blockchain/hardhat.config.js
@@ -0,0 +1,51 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+    }
+  },
+  etherscan: {
+    apiKey: {
+        rinkeby: ETHERSCAN_API_KEY,
+    },
+},
+  gasReporter: {
+    enabled: false, 
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true
+  },
+  solidity: "0.8.7",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 40000
+  }
+};
