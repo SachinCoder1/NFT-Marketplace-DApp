362d8e19430e7cbe51d4539c33458b5ec915e440
SachinCoder1 <sachinhlo232@gmail.com>
2022-07-24 16:08:47 +0530

    getter functions, buy Item

--- blockchain/contracts/NFTMarketplace.sol
+++ blockchain/contracts/NFTMarketplace.sol
@@ -27,7 +27,7 @@ contract NFTMarketplace is ReentrancyGuard {
     /* Structs */
 
     struct Item {
-        uint256 itemId;
+        uint itemId;
         address nftContract;
         uint256 tokenId;
         address payable seller;
@@ -42,7 +42,7 @@ contract NFTMarketplace is ReentrancyGuard {
     /* Events */
 
     event ItemList(
-        uint256 indexed itemId,
+        uint indexed itemId,
         address indexed nftAddress,
         uint256 indexed tokenId,
         address seller,
@@ -53,6 +53,81 @@ contract NFTMarketplace is ReentrancyGuard {
 
     /* Logics */
 
+    function getListingPrice() external view returns(uint256) {
+        return listingPrice;
+    }
+
+    // Get all Listed Items
+    function getAllListedItems() external view returns (Item[] memory) {
+        uint itemCount = s_nftIds.current();
+        uint unSoldItemsCount = s_nftIds.current() - s_nftSold.current();
+        uint currentIndex = 0;
+
+        Item[] memory items = new Item[](unSoldItemsCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if (Items[i + 1].owner == address(0)) {
+                uint currentId = Items[i + 1].itemId;
+                Item storage currentItem = Items[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    // Get Items of the owner who have purchased the items;
+    function getOwnerListedItems() external view returns (Item[] memory) {
+        uint totalListedItems = s_nftIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint256 i = 0; i < totalListedItems; i++) {
+            if (Items[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        Item[] memory items = new Item[](itemCount);
+        for (uint i = 0; i < totalListedItems; i++) {
+            if (Items[i + 1].owner == msg.sender) {
+                uint currentId = Items[i + 1].itemId;
+                Item storage currentItem = Items[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    // Get Items of the seller who have listed items;
+    function getSellerListedItems() external view returns (Item[] memory) {
+        uint totalListedItems = s_nftIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint256 i = 0; i < totalListedItems; i++) {
+            if (Items[i + 1].seller == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        Item[] memory items = new Item[](itemCount);
+        for (uint i = 0; i < totalListedItems; i++) {
+            if (Items[i + 1].seller == msg.sender) {
+                uint currentId = Items[i + 1].itemId;
+                Item storage currentItem = Items[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+
+// List a item;
     function listItem(
         address _nftAddress,
         uint256 _tokenId,
@@ -63,7 +138,7 @@ contract NFTMarketplace is ReentrancyGuard {
         }
 
         s_nftIds.increment();
-        uint256 newNftId = s_nftIds.current();
+        uint newNftId = s_nftIds.current();
 
         Items[newNftId] = Item(
             newNftId,
@@ -88,4 +163,25 @@ contract NFTMarketplace is ReentrancyGuard {
         );
     }
 
+    // Buy Item
+    function buyItem(address _nftAddress, uint256 _itemId)
+        external
+        payable
+        nonReentrant
+    {
+        uint256 price = Items[_itemId].price;
+        uint256 tokenId = Items[_itemId].tokenId;
+        address payable seller = Items[_itemId].seller;
+        if (msg.value != price) {
+            revert NFTMarketplace__ItemPriceNotMet();
+        }
+
+        // Items[_itemId].seller.transfer(msg.value);
+        seller.transfer(msg.value);
+        IERC721(_nftAddress).transferFrom(address(this), msg.sender, tokenId);
+        Items[_itemId].owner = payable(msg.sender);
+        Items[_itemId].sold = true;
+        s_nftSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
 }
