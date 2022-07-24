6447e3148ec54beeec02ff4edbb713b6cc4fa7dd
SachinCoder1 <sachinhlo232@gmail.com>
2022-07-24 13:56:47 +0530

    NFT contract using openzeppelin

---
+++ blockchain/contracts/NFT.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+/* Imports */
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+
+    /* State Variables */
+
+
+    using Counters for Counters.Counter;
+    Counters.Counter private s_tokenIds; 
+    address s_marketplaceAddress;
+
+    constructor(address _marketplaceAddress) ERC721("MyFirstNFT", "nft") {
+     s_marketplaceAddress = _marketplaceAddress;
+    }
+
+
+    /*  Logics */
+
+    function mintToken(string memory _tokenURI) external returns (uint) {
+        s_tokenIds.increment();
+        uint256 newTokenId = s_tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+
+        _setTokenURI(newTokenId, _tokenURI);
+
+        setApprovalForAll(s_marketplaceAddress, true);
+        return newTokenId;
+    }
+
+
+
+ 
+}
